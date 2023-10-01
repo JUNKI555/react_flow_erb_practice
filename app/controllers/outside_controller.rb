ccaab35341d99fdb75d928003c5274a2cc9e95b5
JUNKI555 <junki555@gmail.com>
2023-10-02 03:11:47 +0900

    :pencil2: js fetch to Rails

--- app/controllers/outside_controller.rb
+++ app/controllers/outside_controller.rb
@@ -1,4 +1,8 @@
 class OutsideController < ApplicationController
   def test
   end
+
+  def get_data_json
+    render:json => { :name => "John", :age => 30, :city => "New York" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'outside/test'
+  get 'outside/js_test'
+  get 'outside/get_data_json'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
